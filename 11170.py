da7521d49ba218710c9bc58bfb5f9563c0092b29
MINIBEEF <rjsdid7325@naver.com>
2020-09-10 02:48:23 +0900

    2020 09 10 11170

---
+++ 11170.py
@@ -0,0 +1,20 @@
+def solve(nm_set):
+    for i in range(len(nm_set)):
+        n = int(nm_set[i][0])
+        m = int(nm_set[i][1])
+        cnt = 0
+
+        for num in range(n, m + 1):
+            cnt += str(num).count('0')
+
+        print(cnt)
+
+
+
+if __name__ == '__main__':
+    t = int(input())
+    input_set = list()
+    for i in range(t):
+        input_set.append(input().split())
+
+    solve(input_set)
