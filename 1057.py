f1086a0ce5f624964525d8f80b117add34adcae5
Min Park <MINIBEEF>
2020-09-14 13:57:00 +0900

    2020 09 14 1057

---
+++ 1057.py
@@ -0,0 +1,17 @@
+# problem : https://www.acmicpc.net/problem/1057
+
+def solve(A, B, cnt):
+    if A == B:
+        return cnt
+
+    else:
+        A = (A + 1) // 2
+        B = (B + 1) // 2
+        cnt += 1
+        return solve(A, B, cnt)
+
+N, A, B = input().split()
+A = int(A)
+B = int(B)
+
+print(solve(A, B, 0))
